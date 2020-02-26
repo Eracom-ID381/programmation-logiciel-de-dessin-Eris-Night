650fc23d69102247329908ca371e5f1c5daddee4
Eris-Night <Eris-Night@users.noreply.github.com>
2020-02-26 16:07:13 +0100

    On appuit pour dessiner
    maintient fonctionne et dessin noir sur blanc

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,18 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-  noStroke();
+    createCanvas(windowWidth, windowHeight); // variable de javascript en général, ca défini que notre canvas prendra la taille de l'écran dans le quel on l'ouvre
+    background(255);
+    noStroke();
 }
 
 function draw() {
-  fill(255);
-  rect(mouseX, mouseY, 50, 50);
+    fill(0);
+    if (mouseIsPressed) {
+        circle(mouseX, mouseY, 50);
+    }
+
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  background(0);
+function windowResized() { // change la taille du canvas quand on change la taille de la fenêtre
+    resizeCanvas(windowWidth, windowHeight);
+    background(255); // petit bug qui fait qu'on est obligé de remettre le background
 }
