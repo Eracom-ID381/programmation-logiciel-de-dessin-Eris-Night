7726e188b39b7290b6c7d15874065f7b18de3786
Eris-Night <Eris-Night@users.noreply.github.com>
2020-03-04 15:08:18 +0100

    Ajout des touches de modification de taille du pinceau

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,5 @@
 let strokeVal = 0;
+let strokeValW = 50;
 
 function setup() {
     createCanvas(windowWidth, windowHeight); // variable de javascript en général, ca défini que notre canvas prendra la taille de l'écran dans le quel on l'ouvre
@@ -8,7 +9,7 @@ function setup() {
 
 function draw() {
     stroke(strokeVal); //c'est le contour des formes
-    strokeWeight(50);
+    strokeWeight(strokeValW);
     if (mouseIsPressed === true) {
         line(mouseX, mouseY, pmouseX, pmouseY);
     }
@@ -21,31 +22,37 @@ function keyPressed() {
     } else if (keyCode === BACKSPACE) {
         strokeVal = 255;
     } //pour effacer, donc si c'est pas en blanc bah ca le met en blanc, si non en noir
-    if (keyCode === 82) {
+    if (keyCode === 38) { //flèche du haut
+        strokeValW = strokeValW + 1;
+    }
+    if (keyCode === 40) { //flèche du bas
+        strokeValW = strokeValW - 1;
+    } //Ses touches sont pour agrandir ou diminuer la taille du pinceau de dessin
+    if (keyCode === 82) { //R
         strokeVal = 'red';
     }
-    if (keyCode === 68) {
+    if (keyCode === 68) { //D
         strokeVal = 'black';
     }
-    if (keyCode === 66) {
+    if (keyCode === 66) { //B
         strokeVal = 'blue';
     }
-    if (keyCode === 71) {
+    if (keyCode === 71) { //G
         strokeVal = 'green';
     }
-    if (keyCode === 80) {
+    if (keyCode === 80) { //P
         strokeVal = 'pink';
     }
-    if (keyCode === 67) {
+    if (keyCode === 67) { //C
         strokeVal = 'cyan';
     }
-    if (keyCode === 89) {
+    if (keyCode === 89) { //Y
         strokeVal = 'yellow';
     }
-    if (keyCode === 79) {
+    if (keyCode === 79) { //O
         strokeVal = 'orange';
     }
-    if (keyCode === 86) {
+    if (keyCode === 86) { //V
         strokeVal = 'purple';
     }
 }
