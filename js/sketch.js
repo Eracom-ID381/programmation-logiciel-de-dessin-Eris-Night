f17d1ea6c7167d6edaff430e08b1c1d7f99f6d02
Eris-Night <Eris-Night@users.noreply.github.com>
2020-03-04 14:26:52 +0100

    bouton effacer ajouté

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,5 @@
+let strokeVal = 0;
+
 function setup() {
     createCanvas(windowWidth, windowHeight); // variable de javascript en général, ca défini que notre canvas prendra la taille de l'écran dans le quel on l'ouvre
     background(255);
@@ -5,13 +7,26 @@ function setup() {
 }
 
 function draw() {
-    stroke(0);
+    stroke(strokeVal); //c'est le contour des formes
     strokeWeight(50);
     if (mouseIsPressed === true) {
         line(mouseX, mouseY, pmouseX, pmouseY);
     }
 
 }
+
+function keyPressed() {
+    if (strokeVal === 255 && keyCode === BACKSPACE) {
+        strokeVal = 0;
+    } else if (keyCode === BACKSPACE) {
+        strokeVal = 255;
+    } //pour effacer, donc si c'est pas en blanc bah ca le met en blanc, si non en noir
+
+
+
+
+}
+
 
 function windowResized() { // change la taille du canvas quand on change la taille de la fenêtre
     resizeCanvas(windowWidth, windowHeight);
