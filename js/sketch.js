688ce2ea4cb5e085cd8e8ad489a697a58e65d0d6
Eris-Night <Eris-Night@users.noreply.github.com>
2020-03-11 15:26:08 +0100

    ajout de l'outil des traits

--- js/sketch.js
+++ js/sketch.js
@@ -1,16 +1,29 @@
-let strokeVal = 0;
-let strokeValW = 10;
+let strokeVal = 0; //couleur du trait
+let strokeValW = 10; //taille du trait
+let distribution = new Array(200); //nmb de ligne qui va apparaître
 
 function setup() {
     createCanvas(windowWidth, windowHeight); // variable de javascript en général, ca défini que notre canvas prendra la taille de l'écran dans le quel on l'ouvre
     background(255);
+    for (let i = 0; i < distribution.length; i++) { //distribution c'est pour avoir de manière aléatoire des traits de taille différente
+        distribution[i] = floor(randomGaussian(0, 1000));
+    }
 }
 
 function draw() {
     stroke(strokeVal); //c'est le contour des formes
     strokeWeight(strokeValW); //taille du trait
-    if (mouseIsPressed === true) {
-        line(mouseX, mouseY, pmouseX, pmouseY);
+    if (keyCode === 221 && mouseIsPressed === true) {
+        translate(mouseX, mouseY, pmouseX, pmouseY);
+        for (let i = 0; i < distribution.length; i++) {
+            rotate(TWO_PI / distribution.length);
+            stroke(strokeVal); //la couleur sera la même que celle qui est active
+            strokeWeight(1); //épaisseur du trait
+            let dist = abs(distribution[i]);
+            line(40, 40, 500, 40); //défini le point central d'ou partent les trait, si on agrandit les valeur il y aura un cercle vide au milieu
+        } //dans la troisième variable si je met “dist” ca permet de faire en sorte que les traits ai des tailles différente de manière aléatoire, voir au dessus
+    } else if (mouseIsPressed === true) {
+        line(mouseX, mouseY, pmouseX, pmouseY); //fait la ligne basique pour le dessin
     }
     cursor('crosshair'); //change le curseur
 }
